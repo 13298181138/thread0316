6aee8cc76ad902bc88eb3ae2c29238df0adb3a52
DB <13298181138@users.noreply.github.com>
2017-08-04 00:01:36 +0800

    Update TestGit.java

--- src/com/atguigu/test/TestGit.java
+++ src/com/atguigu/test/TestGit.java
@@ -1,9 +1,11 @@
-package com.atguigu.test;
+	package com.atguigu.test;
 
 public class TestGit {
 	public static void main(String[] args) {
 		System.out.println("0316");
 		System.out.println("样");
 		System.out.println("3");
+System.out.println("4");
+
 	}
 }
